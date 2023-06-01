75f14799d669ec9bebbc3a1961c78be53080944f
msieben <4319079+msieben@users.noreply.github.com>
2023-06-01 12:50:35 +0000

    [Tests/unit/core] : Redesign test_databuffer.
    Use ScopedStorage instead of obsolete CyclicDataBuffer<ScopedStorage>.

--- Tests/unit/core/test_databuffer.cpp
+++ Tests/unit/core/test_databuffer.cpp
@@ -22,46 +22,18 @@
 #include <gtest/gtest.h>
 #include <core/core.h>
 
+#include <cstring>
+
 using namespace WPEFramework;
 
 TEST(Core_DataBuffer, simpleSet)
 {
-    uint32_t bufferSize = 10;    
-    uint32_t size;
-    uint8_t* received = new uint8_t[bufferSize + 1];
-    memset(received, 0, (bufferSize + 1));
+    Core::ScopedStorage<10u> buffer;
 
-    Core::CyclicDataBuffer<Core::ScopedStorage<10>> buffer;
-    EXPECT_TRUE(buffer.IsEmpty());
-    EXPECT_EQ(buffer.Filled(), 0u);
-    EXPECT_EQ(buffer.Free(), bufferSize);
+    EXPECT_EQ(buffer.Size(), 10u);
+    EXPECT_NE(buffer.Buffer(), nullptr);
+    EXPECT_EQ(*buffer.Buffer(), '\0');
 
-    string data = "abcdefghi";
-    size = buffer.Write((uint8_t*)data.c_str(), data.size());
-    EXPECT_EQ(buffer.Filled(), 9u);
-    EXPECT_EQ(buffer.Free(), 1u);
-    EXPECT_EQ(size, data.size());
-
-    size = buffer.Read(received, 4);
-    received[size] = '\0';
-    EXPECT_STREQ((char*)received, "abcd");
-    EXPECT_EQ(buffer.Filled(), 5u);
-    EXPECT_EQ(buffer.Free(), 5u);
-    EXPECT_EQ(size, 4u);
-
-    data = "jklmnopq";
-    size = buffer.Write((uint8_t*)data.c_str(), data.size());
-    EXPECT_EQ(buffer.Filled(), 9u);
-    EXPECT_EQ(buffer.Free(), 1u);
-    EXPECT_EQ(size, data.size());
-
-    size = buffer.Read((uint8_t*)received, buffer.Filled());
-    received[size] = '\0';
-    EXPECT_STREQ((char*)received, "ijklmnopq");
-    EXPECT_EQ(buffer.Filled(), 0u);
-    EXPECT_EQ(buffer.Free(), 10u);
-    EXPECT_EQ(size, 9u);
-
-    delete[] received;
-    Core::Singleton::Dispose();
+    const std::string data = "abcdefghi";
+    EXPECT_STREQ(reinterpret_cast<char*>(std::memcpy(buffer.Buffer(), data.c_str(), data.size())), data.c_str());
 }
